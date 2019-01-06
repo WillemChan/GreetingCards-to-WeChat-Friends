4113a5173b3e6af387d0e906c58ab502972415ad
Willem_C <Williamsstorm@outlook.com>
2019-01-06 19:11:51 +0800

    获取好友数量

--- Download_HeadImages.py
+++ Download_HeadImages.py
@@ -3,8 +3,8 @@ import os
 
 #Get the QR code to log in to the web version of WeChat
 itchat.auto_login()
-
 friends = itchat.get_friends(update=True)[0:]
+print(len(friends))
 user = friends[0]["NickName"][0:]
 print(user)  #Get the user's name
 
