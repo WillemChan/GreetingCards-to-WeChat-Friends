5adc9f45f7a10e300ed9fc39da9afc0435d9d729
Willem_C <Williamsstorm@outlook.com>
2019-01-07 10:47:33 +0800

    添加了必要的注释

--- Convert_Images_to_Circle.py
+++ Convert_Images_to_Circle.py
@@ -6,15 +6,15 @@ def convert_image_to_circle(jpgfile, outdir):
     ima = Image.open(jpgfile).convert("RGBA")
     size = ima.size
 
-    # 因为是要圆形，所以需要正方形的图片
+    # 剪切为圆形，首先需要正方形的图片
     r2 = min(size[0], size[1])
-    if size[0] != size[1]:
+    if size[0] != size[1]:  # 先判断是否为正方形，不是正方形的画需要先把矩形裁剪为正方形
         imb = Image.new('RGBA', (r2, r2), (255, 255, 255, 0))
-        pima = ima.load()
+        pima = ima.load()  # 像素的访问对象
         pimb = imb.load()
         for i in range(r2):
             for j in range(r2):
-                pimb[i, j] = pima[(size[0] - r2) / 2 + i, (size[1] - r2) / 2 + j]
+                pimb[i, j] = pima[(size[0] - r2) / 2 + i, (size[1] - r2) / 2 + j]  # 矩形的中心点与正方形的中心点重合
     else:
         imb = ima
 
@@ -35,6 +35,7 @@ def convert_image_to_circle(jpgfile, outdir):
     # os.path.splitext()用于将文件名与扩展名分离，否则图片仍然会保存为jpg格式，从而报错
     imc.save(os.path.join(outdir, os.path.basename(os.path.splitext(jpgfile)[0]) + '.png'))
 
-for jpgfile in glob.glob("C:/Users/*.jpg"):  #输入待处理图片所在的文件夹
-    convert_image_to_circle(jpgfile, "C:/Users/")  #输入处理之后的图片导出的路径
+# glob.glob可以返回所有匹配的文件路径列表
+for jpgfile in glob.glob("C:/Users/*.jpg"):  # 输入待处理图片所在的文件夹
+    convert_image_to_circle(jpgfile, "C:/Users/")  # 输入处理之后的图片导出的路径
 print("处理完毕")
