39ae1696658a36a39611d0a837e2ce955fa6323b
Willem_C <Williamsstorm@outlook.com>
2019-01-08 21:00:21 +0800

    添加注释

--- 04 ConvertPPTtoImages.py
+++ 04 ConvertPPTtoImages.py
@@ -7,18 +7,18 @@ import win32com.client
 ppSaveAsJPG = 17
 
 
-def PPTtoJPG(pptfile):
+def PPTtoJPG(pptfile, outdir):
     ppt_app = win32com.client.Dispatch('PowerPoint.Application')
     # 设置打开PPT的时候窗口可见，否则会在后台执行
     ppt_app.Visible = 1
     # 打开PPT文件
     ppt = ppt_app.Presentations.Open(pptfile)
     # 保存图片，第一个参数为保存图片的目录，第二个是保存的格式
-    ppt.SaveAs(pptfile + '_result.jpg', ppSaveAsJPG)
+    ppt.SaveAs(outdir, ppSaveAsJPG)  # Office2019提示报错，可能是版本问题
     ppt_app.Quit()
 
 
 if __name__ == '__main__':
-    for pptfile in glob.glob("C:/Users/Willem/Jupyter_Exercise/*.pptx"):
-        PPTtoJPG(pptfile)
+    for pptfile in glob.glob("C:/Users/*.pptx"):  # 此处输入PPTx文件所在目录
+        PPTtoJPG(pptfile, 'C:/Users/Images')  # 导出图片所在目录
     print("处理完毕")
