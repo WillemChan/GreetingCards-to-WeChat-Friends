638d6da58deeb03c6b9fa43aeb8b41b8f0200d6d
Willem_C <Williamsstorm@outlook.com>
2019-01-08 20:43:03 +0800

    添加注释

--- 04 ConvertPPTtoImages.py
+++ 04 ConvertPPTtoImages.py
@@ -9,8 +9,11 @@ ppSaveAsJPG = 17
 
 def PPTtoJPG(pptfile):
     ppt_app = win32com.client.Dispatch('PowerPoint.Application')
+    # 设置打开PPT的时候窗口可见，否则会在后台执行
     ppt_app.Visible = 1
+    # 打开PPT文件
     ppt = ppt_app.Presentations.Open(pptfile)
+    # 保存图片，第一个参数为保存图片的目录，第二个是保存的格式
     ppt.SaveAs(pptfile + '_result.jpg', ppSaveAsJPG)
     ppt_app.Quit()
 
